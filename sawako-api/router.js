79b8cb81cd5e4d130315efd1b82e5dcad2e9be0e
Sawako Ishida <sawako.yoriko@gmail.com>
2016-03-16 11:22:51 -0700

    added both /get and /get/:id route, working

--- sawako-api/router.js
+++ sawako-api/router.js
@@ -11,24 +11,42 @@ mongoose.connect(DB_PORT);
 
 GemologyRouter.use(bodyParser.json());
 
+GemologyRouter.use((err, req, res, next)=>{
+  console.error(err.stack);
+  res.status(500).send('Somthing broke');
+  next();
+});
+
 GemologyRouter.use((req, res, next)=>{
   console.log('Time : ' + new Date());
   next();
 });
 
 GemologyRouter.get('/gems', (req, res)=>{
-
-  res.end();
+  Gem.find({}, (err,gem)=>{
+    if(err){
+      console.log(err);
+    }
+    console.log('Here is gem in get : ' + gem);
+    res.json({data: gem});
+    res.end();
+  });
 });
 
+GemologyRouter.get('/gems/:id', (req, res)=>{
+  // console.log('ID : ' + req.params.id);
+  Gem.find({'_id': req.params.id}, (err, gem)=>{
+    res.json({_id: gem});
+    console.log('inside of :id gem : ' + gem);
+    res.end();
+  });
+});
 
 GemologyRouter.post('/gems', (req, res)=>{
   console.log('This is req.body : ' + req.body);
   var newGem = new Gem(req.body);
-  console.log('This is a newGem : ' + newGem);
   newGem.save((err, gem)=>{
     console.log('It\'s hitting post route' + gem);
-    console.log(err);
     res.json(gem);
     res.end();
   });
